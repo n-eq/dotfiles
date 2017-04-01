46a5b50c4fe9ef843ddfc7b31732132be63c66d0
Nabil Elqatib <nabilelqatib@gmail.com>
2017-04-01 15:42:39 +0200

    Update

---
+++ .vim.symlink/syntax/test.c
@@ -0,0 +1,19 @@
+/********************************************************
+ *
+ * File: test.c
+ * Author: Nabil Elqatib
+ * Email: nabilelqatib@gmail.com
+ * Date: 01/04/2017
+ * Description: 
+ *
+ *******************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+	return EXIT_SUCCESS;
+}
