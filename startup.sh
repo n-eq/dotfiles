ba80ae9acd470b4cee075ff1d2648dbe7812d20a
marrakchino <nabilelqatib@gmail.com>
2017-08-04 22:34:33 +0200

    Sh doesn't support arrays, change shebang to bash

--- startup.sh
+++ startup.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # The main goal of this file is to be used when a brand-new computer has to be 
 # quickly set up. It performs basic packages installation (git, ssh, pdflatex...).
 # This file should be executed with root privileges.
 
 # list of packages to install
-packages=("git" "ssh" "texlive" "openjdk-7-jdk" "gcc" "curl" "libcurl3" "python"\
-    "python-pip")
+packages=("git" "ssh" "texlive" "openjdk-7-jdk" "gcc" "curl" "libcurl3" "python" "python-pip")
 
 for pack in "${packages[@]}"; do
     echo "[startup.sh] Installing ${pack}"
