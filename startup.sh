11377e7f6c4b85eca809ef5c745dc0bd3963b847
marrakchino <nabilelqatib@gmail.com>
2017-08-07 20:40:47 +0200

    Add bicon installation routine and some new packages

--- startup.sh
+++ startup.sh
@@ -2,10 +2,16 @@
 
 # The main goal of this file is to be used when a brand-new computer has to be 
 # quickly set up. It performs basic packages installation (git, ssh, pdflatex...).
-# This file should be executed with root privileges.
-
+# This file may need to be executed with root privileges.
 
 # TODO: install pip packages (grip.. )
+# TODO: install npm packages (cloc.. )
+
+install_bicon(){
+    cd /tmp
+    git clone https://github.com/behdad/bicon && cd bicon
+    ./autogen.sh && ./configure && make && make install
+}
 
 install_only_missing=false
 if [[ "${1}" == "--missing" ]]; then
@@ -13,12 +19,12 @@ if [[ "${1}" == "--missing" ]]; then
 fi
 
 # list of packages to install
-packages=("git" "ssh" "texlive" "gcc" "curl" "libcurl3" "python" "python-pip" "ruby" "libfribidi0" "libfribidi-dev")
+packages=("git" "sudo" "ssh" "texlive" "gcc" "curl" "libcurl3" "python" "python-pip" "ruby" "libfribidi0" "libfribidi-dev" "nodejs" "npm" "pkg-configure" "dh-autoreconf")
 counter=0
 
 for pack in "${packages[@]}"; do
     if [[ "${install_only_missing}" ]]; then
-        pack_found=$(dpkg-query -W --showformat="${Status}\n" "${pack}")
+        pack_found=$(dpkg-query -W --showformat="${Status}\n" "${pack}") # XXX: maybe just use 'which'??
         if [[ "${pack_found}" != "" ]]; then
             echo "[startup.sh] Installing missing package: ${pack}"
             apt install "${pack}" --assume-yes
@@ -37,3 +43,7 @@ echo "[startup.sh] Installed ${counter} packages."
 echo "[startup.sh] updating..."
 # apt update 
 # apt upgrade
+
+# TODO: test this
+# echo "[startup.sh] installing bicon.."
+# install_bicon
