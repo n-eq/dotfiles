5cd40bd39f8c63400901bcda746285d42a96abf1
marrakchino <nabilelqatib@gmail.com>
2017-08-07 19:55:20 +0200

    Update startup script
    * Add libfribidi packages
    
    * Add installed packages counter
    
    * Add --missing option to skip already installde packages (dpkg-query)

--- startup.sh
+++ startup.sh
@@ -7,13 +7,33 @@
 
 # TODO: install pip packages (grip.. )
 
+install_only_missing=false
+if [[ "${1}" == "--missing" ]]; then
+    install_only_missing=true
+fi
+
 # list of packages to install
-packages=("git" "ssh" "texlive" "openjdk-7-jdk" "gcc" "curl" "libcurl3" "python" "python-pip" "ruby")
+packages=("git" "ssh" "texlive" "gcc" "curl" "libcurl3" "python" "python-pip" "ruby" "libfribidi0" "libfribidi-dev")
+counter=0
 
 for pack in "${packages[@]}"; do
-    echo "[startup.sh] Installing ${pack}"
-    apt install "${pack}" --assume-yes
+    if [[ "${install_only_missing}" ]]; then
+        pack_found=$(dpkg-query -W --showformat="${Status}\n" "${pack}")
+        if [[ "${pack_found}" != "" ]]; then
+            echo "[startup.sh] Installing missing package: ${pack}"
+            apt install "${pack}" --assume-yes
+            counter=$(( counter + 1 ))
+        else
+            echo "[startup.sh] Package ${pack} found, skipped."
+        fi
+    else
+        echo "[startup.sh] Installing ${pack}"
+        apt install "${pack}" --assume-yes
+        counter=$(( counter + 1 ))
+    fi
 done
+echo "[startup.sh] Installed ${counter} packages."
 
-apt update 
-apt upgrade
+echo "[startup.sh] updating..."
+# apt update 
+# apt upgrade
